d7ff854c19fbd2d8750bd23b8ac06b016eda8669
JaviMera <javier_mera88@hotmail.com>
2021-09-12 14:23:20 -0500

    Updated loading button to draw text in the center of the button, and made the text a custom attribute.

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,10 +2,11 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,20 +14,44 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
-
     private val valueAnimator = ValueAnimator()
+    private lateinit var downloadText: String
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
-
     init {
-    }
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.LoadingButton,
+            0,0).apply {
 
+                downloadText = getString(R.styleable.LoadingButton_text)!!
+        }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        val paint = Paint().apply {
+            color = ContextCompat.getColor(context ,R.color.colorPrimary)
+        }
+
+        val textPaint = TextPaint().apply {
+            color = Color.WHITE
+            textAlign = Paint.Align.CENTER
+            textSize = resources.getDimension(R.dimen.custom_button_text_size)
+        }
+
+        val textHeight = textPaint.descent() - textPaint.ascent()
+        val textOffset = (textHeight / 2) - textPaint.descent()
+        val bounds = Rect(0,0, width, height)
+
+        canvas?.drawRect(bounds, paint)
+        canvas?.drawText(downloadText,
+            bounds.centerX() * 1f,
+            bounds.centerY() * 1f + (textOffset), textPaint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
